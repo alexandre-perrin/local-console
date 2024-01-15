bd3553afb5e1a6a0fcb607017fb0a76810f90db2
Luis Linares <luis@midokura.com>
2024-01-15 16:28:56 +0100

    chore(build): Parametrize module memory limits
    These limits are crucial to get right when building a module
    to be deployed on a small device such as the Type 3 camera.
    The new parameters enable further parametrization later on
    (e.g. exposing the limits as options in the `wedge build`
    command)>

--- samples/sdk/rules.mk
+++ samples/sdk/rules.mk
@@ -2,10 +2,18 @@ WASI_SDK_PATH ?= /opt/wasi-sdk
 CC = $(WASI_SDK_PATH)/bin/clang
 CXX = $(WASI_SDK_PATH)/bin/clang++
 
+# References about WASM linear memory handling by WAMR:
+# - https://bytecodealliance.github.io/wamr.dev/blog/the-wamr-memory-model/
+# - https://bytecodealliance.github.io/wamr.dev/blog/understand-the-wamr-stacks/
+# - https://github.com/bytecodealliance/wasm-micro-runtime/blob/main/doc/build_wasm_app.md#1-wasi-sdk-options
+# - https://github.com/bytecodealliance/wasm-micro-runtime/blob/main/doc/memory_tune.md
+MODULE_MAX_LINEAR_MEM_KB = 128
+MODULE_AUX_STACK_KB = 32
+
 PROJ_LDFLAGS = \
 	-Wl,-allow-undefined \
-	-z stack-size=32768 \
-	-Wl,--initial-memory=655360 \
+	-z stack-size=$$(( $(MODULE_AUX_STACK_KB) * 1024 )) \
+	-Wl,--max-memory=$$(( $(MODULE_MAX_LINEAR_MEM_KB) * 1024 )) \
 	-Wl,--export=malloc -Wl,--export=free \
 	-Wl,--export=__data_end -Wl,--export=__heap_base
 
