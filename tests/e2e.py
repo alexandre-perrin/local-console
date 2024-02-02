b72144fc2d4ca176154b8d7bc507cc72e7a5f4d5
Luis Linares <luis@midokura.com>
2024-02-02 13:07:33 +0100

    feat(e2e): run wedge-cli within a temporary directory
    This makes the E2E script invoke all wedge-cli commands within
    a fresh configuration area, such that the E2E test can be
    executed without touching the default area.
    
    Another relevant change is replacing plain print()s with usage
    of the logging module.

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,10 +1,17 @@
 import json
-import os
+import logging
 import subprocess
 import time
+from contextlib import contextmanager
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 import psutil
 from retry import retry
+
+FORMAT = "%(asctime)s %(levelname)s %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+log = logging.getLogger()
 
 
 def kill_agent() -> None:
@@ -14,10 +21,10 @@ def kill_agent() -> None:
 
 
 @retry(tries=5, exceptions=AssertionError)
-def check_rpc_telemetry(telemetry: subprocess.Popen) -> None:
+def check_rpc_telemetry(telemetry: subprocess.Popen, wedge_cli_pre: list[str]) -> None:
     # send rpc
     # 000FF1 = (0,15,241)
-    subprocess.run(["wedge-cli", "rpc", "node", "my-method", '{"rgb":"000FF1"}'])
+    subprocess.run(wedge_cli_pre + ["rpc", "node", "my-method", '{"rgb":"000FF1"}'])
     time.sleep(2)
     for line in telemetry.stdout:  # type: ignore
         telemetry_out = json.loads(line.replace("'", '"'))
@@ -33,12 +40,15 @@ def check_rpc_telemetry(telemetry: subprocess.Popen) -> None:
 
 
 @retry(tries=5, exceptions=AssertionError)
-def check_deploy_empty(deployment: subprocess.Popen) -> None:
-    subprocess.run(
-        ["wedge-cli", "-v", "deploy", "-e"],
-        capture_output=True,
-        text=True,
-    )
+def check_deploy_empty(deployment: subprocess.Popen, wedge_cli_pre: list[str]) -> None:
+    try:
+        subprocess.run(
+            wedge_cli_pre + ["deploy", "-e"], capture_output=True, text=True, check=True
+        )
+    except subprocess.CalledProcessError as e:
+        log.error("Deploying failed like: %s", e.stderr)
+        raise e
+
     time.sleep(2)
     for line in deployment.stdout:  # type: ignore
         assert "'instances': {}, 'modules': {}" in line
@@ -46,57 +56,64 @@ def check_deploy_empty(deployment: subprocess.Popen) -> None:
         break
 
 
-def start_deploy_build() -> None:
-    env = os.environ.copy()
-    # start_agent
-    subprocess.Popen(["wedge-cli", "start"], env=env)
-    # build and deploy, wait necessary to give time for wasms to build
-    os.chdir("samples/rpc-example")
-    subprocess.run(["wedge-cli", "-v", "build"])
-    time.sleep(3)
+def build_and_deploy_app(app_dir: Path, wedge_cli_pre: list[str]) -> None:
+    # Build app
+    subprocess.run(wedge_cli_pre + ["build"], cwd=app_dir, check=True)
+
+    # Deploy app
     deploy = subprocess.run(
-        ["wedge-cli", "-v", "deploy", "--timeout", "60"],
+        wedge_cli_pre + ["deploy", "--timeout", "60"],
+        cwd=app_dir,
         capture_output=True,
         text=True,
+        check=True,
     )
     assert "WARNING" not in deploy.stdout
     assert deploy.returncode == 0, f"Error during deploy: {deploy.stderr}"
 
 
+@contextmanager
+def wedge_area() -> None:
+    with TemporaryDirectory() as _tempdir:
+        tmp_dir = Path(_tempdir)
+        config_dir = tmp_dir / "config"
+        cmd_preamble = ["wedge-cli", "-d", "--config-dir", str(config_dir)]
+
+        yield tmp_dir, cmd_preamble
+
+
 def main() -> None:
-    try:
-        start_deploy_build()
-    except OSError:
-        kill_agent()
-        raise Exception("Test failed: before deployment arrived")
-    except AssertionError:
-        kill_agent()
-        raise Exception("Deployment timed out before arriving")
+    app_dir = Path("samples/rpc-example")
 
     try:
-        # get telemetry - check
-        telemetry = subprocess.Popen(
-            ["wedge-cli", "get", "telemetry"],
-            stdout=subprocess.PIPE,
-            universal_newlines=True,
-        )
+        with (wedge_area() as (tmp_dir, cmd_preamble),):
+            build_and_deploy_app(app_dir, cmd_preamble)
+            log.info("Deployed module successfully")
 
-        check_rpc_telemetry(telemetry)
-        print("Telemetry arrived succesfully")
+            telemetry = subprocess.Popen(
+                cmd_preamble + ["get", "telemetry"],
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+            check_rpc_telemetry(telemetry, cmd_preamble)
+            log.info("Telemetry arrived succesfully")
 
-        deployment = subprocess.Popen(
-            ["wedge-cli", "get", "deployment"],
-            stdout=subprocess.PIPE,
-            universal_newlines=True,
-        )
+            deployment = subprocess.Popen(
+                cmd_preamble + ["get", "deployment"],
+                stdout=subprocess.PIPE,
+                text=True,
+            )
+            check_deploy_empty(deployment, cmd_preamble)
+            log.info("Deployment emptied successfully")
 
-        check_deploy_empty(deployment)
-    except Exception:
+        log.info("Test successful!")
+
+    except subprocess.CalledProcessError as e:
+        log.error("Execution failed: %s", e)
+    except KeyboardInterrupt:
+        log.info("Cancelling")
+    finally:
         kill_agent()
-
-    kill_agent()
-    print("Deployment empty")
-    print("Test successful!")
 
 
 if __name__ == "__main__":
