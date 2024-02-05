b9b4ce24850e7eee0853a7e11910b9f9341ad2c1
Luis Linares <luis@midokura.com>
2024-02-05 16:16:32 +0100

    feat(tls): make the agent connect over TLS
    The `wedge-cli start` command now uses the TLS machinery,
    with some relevant additions, to enable the agent connect
    to the MQTT broker over TLS, even if the broker is also
    running in the same host (localhost). This particular
    scenario is made possible by using cwrap's nss_wrapper [1],
    which makes it possible for the agent to resolve localhost
    to whatever arbitrary Common Name (CN) is present in the
    server's subject of its TLS certificate. The server CN is
    inquired on the fly from the server, just before launching
    the agent.

--- tests/unit/commands/test_start.py
+++ tests/unit/commands/test_start.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+from uuid import UUID
 
 import hypothesis.strategies as st
 from hypothesis import given
@@ -12,8 +13,10 @@ from wedge_cli.utils.schemas import AgentConfiguration
 from wedge_cli.utils.schemas import IPAddress
 from wedge_cli.utils.schemas import Libraries
 from wedge_cli.utils.schemas import RemoteConnectionInfo
+from wedge_cli.utils.schemas import TLSConfiguration
 
 from tests.strategies.configs import generate_agent_config
+from tests.strategies.configs import generate_tls_config
 from tests.strategies.configs import generate_valid_ip
 from tests.strategies.configs import generate_valid_port_number
 
@@ -124,12 +127,85 @@ def test_start_agent_file_not_found(agent_config: AgentConfiguration) -> None:
         assert mock_run_agent.call_args.kwargs == dict(env=env, check=True)
 
 
+class MockTemporaryDirectory:
+    def __init__(self, given_path):
+        self.given_path = given_path
+
+    def __enter__(self):
+        return self.given_path
+
+    def __call__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+
+
+@given(generate_agent_config(), generate_tls_config(), st.uuids())
+def test_start_agent_with_tls(
+    agent_config: AgentConfiguration, tls_config: TLSConfiguration, random_name: UUID
+) -> None:
+    with (
+        patch("wedge_cli.commands.start.run") as mock_run_agent,
+        patch(
+            "wedge_cli.commands.start.get_config", return_value=agent_config
+        ) as mock_get_config,
+        patch("os.environ.copy", return_value=dict()),
+        patch("shutil.which", return_value=Commands.EVP_AGENT.value),
+        patch("ctypes.cdll.LoadLibrary"),
+        patch(
+            "wedge_cli.commands.start.TemporaryDirectory",
+            new=MockTemporaryDirectory(f"/tmp/{random_name}"),
+        ),
+        patch("pathlib.Path.open"),
+        patch("wedge_cli.commands.start.is_localhost", return_value=True),
+        patch(
+            "wedge_cli.commands.start.ensure_tls_setup", return_value="brokerhost"
+        ) as mock_ensure_tls,
+        patch("wedge_cli.commands.start.is_localhost", return_value=True),
+        patch("wedge_cli.commands.start.ensure_certificate_pair_exists"),
+    ):
+        agent_config.tls = tls_config
+
+        rc = start_agent(
+            connection_info=RemoteConnectionInfo(host=None, port=None),
+            libraries=Libraries(libraries=[]),
+        )
+        command = [Commands.EVP_AGENT.value]
+
+        env = dict()
+        env[EVPEnvVars.EVP_IOT_PLATFORM] = agent_config.evp.iot_platform
+        env[EVPEnvVars.EVP_MQTT_HOST] = agent_config.mqtt.host.ip_value
+        env[EVPEnvVars.EVP_MQTT_PORT] = str(agent_config.mqtt.port)
+        env[EVPEnvVars.EVP_DATA_DIR] = str(config_paths.evp_data_path)  # type:ignore
+        env[EVPEnvVars.EVP_HTTPS_CA_CERT] = str(
+            config_paths.https_ca_path
+        )  # type:ignore
+        env[EVPEnvVars.EVP_REPORT_STATUS_INTERVAL_MAX_SEC] = "3"
+        env[EVPEnvVars.EVP_MQTT_TLS_CLIENT_CERT] = str(
+            config_paths.tls_cert_root / "agent.crt.pem"
+        )
+        env[EVPEnvVars.EVP_MQTT_TLS_CLIENT_KEY] = str(
+            config_paths.tls_cert_root / "agent.key.pem"
+        )
+        env[EVPEnvVars.EVP_MQTT_TLS_CA_CERT] = str(agent_config.tls.ca_certificate)
+        env["LD_PRELOAD"] = "libnss_wrapper.so"
+        env["NSS_WRAPPER_HOSTS"] = f"/tmp/{random_name}/hosts"
+        env[EVPEnvVars.EVP_MQTT_HOST] = "brokerhost"
+
+        assert rc == 0
+        mock_get_config.assert_called_once()
+        mock_ensure_tls.assert_called_once()
+        mock_run_agent.assert_called_once_with(command, env=env, check=True)
+
+
 @given(generate_valid_ip(), generate_valid_port_number())
 def test_start_agent_remote(valid_ip: str, port: int) -> None:
     with (
         patch("wedge_cli.commands.start.Listener") as mock_listener,
         patch("wedge_cli.commands.start.run") as mock_run_agent,
         patch("wedge_cli.commands.start.get_config") as mock_get_config,
+        patch("wedge_cli.commands.start.is_localhost", return_value=False),
         patch("wedge_cli.commands.start.get_agent_environment") as mock_get_env,
     ):
         start_agent(
