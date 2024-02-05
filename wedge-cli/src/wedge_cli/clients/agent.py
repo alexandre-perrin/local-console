d24b1b0c886085002948cd83ba43e6b3c53d21ee
Luis Linares <luis@midokura.com>
2024-02-05 07:29:10 +0100

    chore(io): switch mqtt driver to asynchronous i/o
    This commit and its ensuing ones make the switch from using
    synchronous I/O for the mqtt client Paho, to use async I/O
    on top of the Trio library. Not all changes are contained here,
    but the ones used by the other classes in the CLI, such as the
    commands. Each command will be updated separately in the next
    commits.

--- wedge-cli/src/wedge_cli/clients/agent.py
+++ wedge-cli/src/wedge_cli/clients/agent.py
@@ -2,16 +2,21 @@ import base64
 import json
 import logging
 import random
-import sys
-import time
+import traceback
 from collections import defaultdict
-from collections.abc import Callable
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
 from typing import Any
+from typing import Callable
+from typing import Optional
 
 import paho.mqtt.client as paho
+import trio
+from exceptiongroup import catch
 from paho.mqtt.client import MQTT_ERR_SUCCESS
 from wedge_cli.utils.config import get_config
 from wedge_cli.utils.schemas import AgentConfiguration
+from wedge_cli.utils.trio_paho_mqtt import AsyncClient
 
 logger = logging.getLogger(__name__)
 
@@ -25,27 +30,18 @@ class Agent:
     TELEMETRY = "v1/devices/me/telemetry"
 
     def __init__(self) -> None:
-        config_parse: AgentConfiguration = get_config()
         self.mqttc = paho.Client()
-        self.mqttc.connect(config_parse.mqtt.host.ip_value, config_parse.mqtt.port)
-        self._on_connect()
+        self.client: Optional[AsyncClient] = None
+        self.nursery: Optional[trio.Nursery] = None
 
-    def _on_connect(self) -> None:
-        mqtt_msg_info = self.mqttc.publish(self.REQUEST_TOPIC, "{}")
-        rc, _ = mqtt_msg_info
-        if rc != MQTT_ERR_SUCCESS:
-            logger.error("Error on MQTT handshake with agent")
+        config_parse: AgentConfiguration = get_config()
+        self._host = config_parse.mqtt.host.ip_value
+        self._port = config_parse.mqtt.port
 
-    def _on_connect_subscribe_callback(self, topic: str) -> Callable:
-        def __callback(
-            client: paho.Client,
-            userdata: Any,
-            flags: dict,
-            rc: str,
-        ) -> None:
-            self.mqttc.subscribe(topic)
+    def async_done(self) -> None:
+        assert self.nursery
+        self.nursery.cancel_scope.cancel()
 
-        return __callback
 
     def _on_message_return_payload(self) -> Callable:
         def __callback(
@@ -168,16 +164,39 @@ class Agent:
             message_callback=self._on_message_logs(instance_id, timeout),
         )
 
-    def _loop_client(
-        self, connect_callback: Callable, message_callback: Callable
+    async def loop_client(
+        self, subs_topics: list[str], driver_task: Callable, message_task: Callable
     ) -> None:
-        self.mqttc.on_connect = connect_callback
-        self.mqttc.on_message = message_callback
-        try:
-            self.mqttc.loop_forever()
-        except KeyboardInterrupt:
-            # avoids ugly logs when killing the loop
-            pass
+        async with self.mqtt_scope(subs_topics):
+            assert self.nursery is not None
+            cs = self.nursery.cancel_scope
+            self.nursery.start_soon(message_task, cs)
+            self.nursery.start_soon(driver_task, cs)
+
+    @asynccontextmanager
+    async def mqtt_scope(self, subs_topics: list[str]) -> AsyncIterator[None]:
+        async with guarded_nursery() as nursery:
+            self.nursery = nursery
+            self.client = AsyncClient(self.mqttc, self.nursery)
+
+            self.client.connect(self._host, self._port)
+            for topic in subs_topics:
+                self.client.subscribe(topic)
+
+            yield
+
+    async def publish(self, topic: str, payload: str) -> None:
+        assert self.client is not None
+        msg_info = await self.client.publish_and_wait(topic, payload=payload)
+        if msg_info[0] != MQTT_ERR_SUCCESS:
+            logger.error("Error on MQTT publish agent logs")
+            raise ConnectionError
+
+    def _loop_forever(self, subs_topics: list[str], message_task: Callable) -> None:
+        async def _driver_task(_cs: trio.CancelScope) -> None:
+            await trio.sleep_forever()
+
+        trio.run(self.loop_client, subs_topics, _driver_task, message_task)
 
     def get_deployment(self) -> None:
         self._loop_client(
@@ -200,3 +219,29 @@ class Agent:
             ),
             message_callback=self._on_message_instance(instance_id),
         )
+
+
+@asynccontextmanager
+async def guarded_nursery() -> AsyncIterator[trio.Nursery]:
+    with catch(
+        {
+            Exception: handle_task_exceptions,
+        }
+    ):
+        async with trio.open_nursery() as nursery:
+            yield nursery
+
+
+def handle_task_exceptions(excgroup: Any) -> None:
+    # The 'Any' annotation is used to silence mypy,
+    # as it is not raising a helpful error.
+    num_exceptions = len(excgroup.exceptions)
+    logger.error(
+        "%d Exception%s occurred, listed below:",
+        num_exceptions,
+        "s" if num_exceptions else "",
+    )
+    for e in excgroup.exceptions:
+        exc_desc_lines = traceback.format_exception_only(type(e), e)
+        exc_desc = "".join(exc_desc_lines).rstrip()
+        logger.error("Exception: %s", exc_desc)
