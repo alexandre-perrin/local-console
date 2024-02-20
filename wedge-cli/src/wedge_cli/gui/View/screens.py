f3d55de5906ec48ca741bb99eae233537f0f7caa
Luis Linares <luis@midokura.com>
2024-02-20 16:32:07 +0100

    feat(gui): add settings screen
    This screen is mostly a skeleton, as currently it is just showing
    the machine's IP address in the local network. However, it is to
    be extended with widgets for accessing the CLI's configuration,
    such as the MQTT connection settings.

--- wedge-cli/src/wedge_cli/gui/View/screens.py
+++ wedge-cli/src/wedge_cli/gui/View/screens.py
@@ -1,12 +1,18 @@
 # The screen's dictionary contains the objects of the models and controllers
 # of the screens of the application.
 from wedge_cli.gui.Controller.home_screen import HomeScreenController
+from wedge_cli.gui.Controller.settings_screen import SettingsScreenController
 from wedge_cli.gui.Model.home_screen import HomeScreenModel
+from wedge_cli.gui.Model.settings_screen import SettingsScreenModel
 
 screens = {
     "home screen": {
         "model": HomeScreenModel,
         "controller": HomeScreenController,
+    },
+    "settings screen": {
+        "model": SettingsScreenModel,
+        "controller": SettingsScreenController,
     },
 }
 
