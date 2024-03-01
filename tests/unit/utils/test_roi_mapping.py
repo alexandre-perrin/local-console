e587f29f692d71d06c07c636ebd7e5a724bb82d4
Luis Linares <luis@midokura.com>
2024-03-01 18:09:33 +0100

    feat(streaming): introduce two ROI types and their mapping
    The two types of ROI specification are raw and normalized:
    - Raw are in integer units for pixels, and the IMX500 sensor
    pixel matrix dimensions are included as the effective set this
    ROI type is based one.
    - Normalized are in floating point units for covering the [0, 1]
    number set, and will be useful for translating between Kivy's
    point coordinate system and the IMX500 ROI specification coordinate
    system.
    
    These types and the associated functions will be applied in the
    next commits.

---
+++ tests/unit/utils/test_roi_mapping.py
@@ -0,0 +1,38 @@
+from wedge_cli.gui.Utility.axis_mapping import as_normal_in_set
+from wedge_cli.gui.Utility.axis_mapping import DEFAULT_ROI
+from wedge_cli.gui.Utility.axis_mapping import pixel_roi_from_normals
+from wedge_cli.gui.Utility.axis_mapping import SENSOR_SIZE
+
+
+def test_normalizing():
+    low = 1
+    high = 3
+    assert as_normal_in_set(2, (low, high)) == 0.5
+    assert as_normal_in_set(low, (low, high)) == 0
+    assert as_normal_in_set(high, (low, high)) == 1
+    assert as_normal_in_set(0, (low, high)) == -0.5
+    assert as_normal_in_set(4, (low, high)) == 1.5
+
+
+def test_normals():
+    low = 0.2
+    high = 0.7
+    assert as_normal_in_set(0.1, (low, high)) < 0
+    assert 0.9 < as_normal_in_set(0.65, (low, high)) < 1
+    assert 0 < as_normal_in_set(0.21, (low, high)) < 0.1
+
+
+def test_unit_roi_denormalization():
+    denorm = pixel_roi_from_normals(DEFAULT_ROI)
+    assert denorm == ((0, 0), (SENSOR_SIZE[0], SENSOR_SIZE[1]))
+
+
+def test_reference_point_denormalization():
+    unit_v_offset = 0.1
+    roi = ((0.5, unit_v_offset), (0.5, 0.5))
+    (h_offset, v_offset), (h_size, v_size) = pixel_roi_from_normals(roi)
+
+    assert h_size == SENSOR_SIZE[0] / 2
+    assert v_size == SENSOR_SIZE[1] / 2
+    assert h_offset == (SENSOR_SIZE[0] / 2 - 1)
+    assert v_offset == (4 * SENSOR_SIZE[1] / 10 - 1)
---
+++ wedge-cli/src/wedge_cli/gui/Utility/axis_mapping.py
@@ -0,0 +1,52 @@
+from typing import Optional
+from typing import Union
+
+# Type of ROI units: raw pixel coordinates
+PixelROI = tuple[tuple[int, int], tuple[int, int]]
+# Type of ROI units: normalized (from 0 to 1) with
+# respect to the pixel matrix dimensions
+UnitROI = tuple[tuple[float, float], tuple[float, float]]
+
+# Fundamental constant: IMX500 pixel matrix dimensions
+SENSOR_SIZE = 4056, 3040
+
+
+def pixel_roi_from_normals(normal_roi: Optional[UnitROI]) -> PixelROI:
+    """
+    This function maps a normalized ROI (in units from 0 to 1)
+    to pixel size coordinates in the IMX500 matrix.
+    """
+    sensor_size = SENSOR_SIZE
+
+    if normal_roi is None:
+        return (0, 0), (sensor_size[0], sensor_size[1])
+
+    n_start, n_size = normal_roi
+
+    h_offset = int(denormalize_in_set(n_start[0], (0, sensor_size[0] - 1)))
+    # Whereas our ROI's start is its lower-left corner,
+    # the camera's ROI start is its upper-left one.
+    v_offset = int(
+        denormalize_in_set(1 - (n_start[1] + n_size[1]), (0, sensor_size[1] - 1))
+    )
+    h_size = int(denormalize_in_set(n_size[0], (0, sensor_size[0])))
+    v_size = int(denormalize_in_set(n_size[1], (0, sensor_size[1])))
+
+    return (h_offset, v_offset), (h_size, v_size)
+
+
+# Typing for the number mapping functions below
+Number = Union[int, float]
+NumberSet = tuple[Number, Number]
+
+
+def as_normal_in_set(value: Number, set_: NumberSet) -> Number:
+    return (value - set_[0]) / (set_[1] - set_[0])
+
+
+def denormalize_in_set(value: Number, set_: NumberSet) -> Number:
+    return set_[0] + (value * (set_[1] - set_[0]))
+
+
+def delta(set_: NumberSet) -> Number:
+    return set_[1] - set_[0]
