d4fe6ed6ca0845839d7920449505a4d557795ccc
Luis Linares <luis@midokura.com>
2024-03-05 15:16:48 +0100

    fix(e2e): only return 0 when the test succeeded
    This should avoid the masking of some types of
    false positives during tests.

--- tests/e2e.py
+++ tests/e2e.py
@@ -3,6 +3,7 @@ import json
 import logging
 import socket
 import subprocess
+import sys
 import time
 from contextlib import contextmanager
 from pathlib import Path
@@ -209,6 +210,7 @@ def main() -> None:
     app_dir = Path("samples/rpc-example")
     with_tls = args.with_tls
 
+    retcode = 1
     try:
         with (
             wedge_area(with_tls) as (tmp_dir, cmd_preamble),
@@ -238,6 +240,7 @@ def main() -> None:
         log.info("######################")
         log.info("#  Test successful!  #")
         log.info("######################")
+        retcode = 0
 
     except (subprocess.CalledProcessError, ValueError) as e:
         log.error("Execution failed: %s", e)
@@ -245,6 +248,8 @@ def main() -> None:
     except KeyboardInterrupt:
         log.info("Cancelling")
 
+    return retcode
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
