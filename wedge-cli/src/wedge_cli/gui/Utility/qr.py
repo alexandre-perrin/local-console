d769f1a00bfa242bed185c2f671a47ce0059164f
Luis Linares <luis@midokura.com>
2024-03-12 19:22:50 +0100

    feat(gui): enable displaying styled QR codes in Image widgets
    This will enable the upcoming Connection screen to display
    the connection settings QR code into an Image widget.

---
+++ wedge-cli/src/wedge_cli/gui/Utility/qr.py
@@ -0,0 +1,36 @@
+import io
+
+import qrcode
+from kivy.core.image import Texture, Image as CoreImage
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.colormasks import SolidFillColorMask
+from qrcode.image.styles.moduledrawers import CircleModuleDrawer
+
+# Color tuple, whose components value range is [0, 255]
+Color = tuple[int, ...]
+
+
+def qr_object_as_texture(
+    qr: qrcode.main.QRCode, background_color: Color, fill_color: Color
+) -> Texture:
+    """
+    This function renders a QR code as a Kivy texture, so that an uix.Image widget
+    can be updated without involving the filesystem
+    :param qr: QR code object
+    :param background_color: Background (i.e. space between cells) color for the QR code
+    :param fill_color: Foreground (i.e. cell) color for the QR code
+    :return: object that can be assigned to the .texture property of a kivy.uix.Image object
+    """
+    img = qr.make_image(
+        image_factory=StyledPilImage,  # type: ignore[type-abstract]
+        module_drawer=CircleModuleDrawer(),
+        color_mask=SolidFillColorMask(
+            front_color=fill_color, back_color=background_color
+        ),
+    )
+    img_data = io.BytesIO()
+    img_ext = "PNG"
+    img.save(img_data, format=img_ext)
+    img_data.seek(0)
+    cim = CoreImage(img_data, ext=img_ext.lower())
+    return cim.texture
