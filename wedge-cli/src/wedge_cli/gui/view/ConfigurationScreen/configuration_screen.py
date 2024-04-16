242508fc2027fe193661e4bc64f5b311a4d22e40
Luis Linares <luis@midokura.com>
2024-04-16 23:26:59 +0200

    refactor(gui): use path selector combo in configuration screen
    It gets massively simplified as a result.

--- wedge-cli/src/wedge_cli/gui/view/ConfigurationScreen/configuration_screen.py
+++ wedge-cli/src/wedge_cli/gui/view/ConfigurationScreen/configuration_screen.py
@@ -7,7 +7,9 @@ from kivymd.uix.snackbar import MDSnackbarButtonContainer
 from kivymd.uix.snackbar import MDSnackbarCloseButton
 from kivymd.uix.snackbar import MDSnackbarSupportingText
 from wedge_cli.gui.view.base_screen import BaseScreenView
-from wedge_cli.gui.view.common.components import FileManager
+from wedge_cli.gui.view.common.components import (
+    PathSelectorCombo,
+)  # nopycln: import # Required by the screen's KV spec file
 
 
 class ConfigurationScreenView(BaseScreenView):
@@ -18,11 +20,11 @@ class ConfigurationScreenView(BaseScreenView):
         according to these changes.
         """
         if self.model.image_directory is not None:
-            self.ids.lbl_image_path.text = str(self.model.image_directory)
+            self.ids.image_pick.accept_path(str(self.model.image_directory))
         if self.model.inferences_directory is not None:
-            self.ids.lbl_inference_path.text = str(self.model.inferences_directory)
+            self.ids.inference_pick.accept_path(str(self.model.inferences_directory))
         if self.model.flatbuffers_schema is not None:
-            self.ids.lbl_schema_path.text = str(self.model.flatbuffers_schema)
+            self.ids.schema_pick.accept_path(str(self.model.flatbuffers_schema))
         if self.model.flatbuffers_process_result is not None:
             self.show_flatbuffers_process_result(self.model.flatbuffers_process_result)
             self.model.flatbuffers_process_result = None
@@ -30,29 +32,21 @@ class ConfigurationScreenView(BaseScreenView):
     def __init__(self, **kargs: Any) -> None:
         super().__init__(**kargs)
 
-        self.file_manager_image = FileManager(
-            exit_manager=self.exit_manager_image,
-            select_path=self.select_path_image,
-            search="dirs",
-        )
-        self.file_manager_inferences = FileManager(
-            exit_manager=self.exit_manager_inferences,
-            select_path=self.select_path_inferences,
-            search="dirs",
-        )
-        self.file_manager_flatbuffers = FileManager(
-            exit_manager=self.exit_manager_flatbuffers,
-            select_path=self.select_path_flatbuffers,
-            ext=[".fbs"],
-        )
+        self.ids.image_pick.file_manager.select_path = self.select_path_image
+        self.ids.image_pick.file_manager.search = "dirs"
+
+        self.ids.inference_pick.file_manager.select_path = self.select_path_inferences
+        self.ids.inference_pick.file_manager.search = "dirs"
+
+        self.ids.schema_pick.file_manager.select_path = self.select_path_flatbuffers
+        self.ids.schema_pick.file_manager.ext = [".fbs"]
 
     def select_path_image(self, path: str) -> None:
         """
         It will be called when the user selects the directory.
         :param path: path to the selected directory;
         """
-        self.exit_manager_image()
-        self.file_manager_image.refresh_opening_path()
+        self.ids.image_pick.file_manager.exit_manager()
         self.controller.update_image_directory(Path(path))
 
     def select_path_inferences(self, path: str) -> None:
@@ -60,8 +54,7 @@ class ConfigurationScreenView(BaseScreenView):
         It will be called when the user selects the directory.
         :param path: path to the selected directory;
         """
-        self.exit_manager_inferences()
-        self.file_manager_inferences.refresh_opening_path()
+        self.ids.inference_pick.file_manager.exit_manager()
         self.controller.update_inferences_directory(Path(path))
 
     def select_path_flatbuffers(self, path: str) -> None:
@@ -69,21 +62,8 @@ class ConfigurationScreenView(BaseScreenView):
         It will be called when the user selects the directory.
         :param path: path to the selected directory;
         """
-        self.exit_manager_flatbuffers()
-        self.file_manager_flatbuffers.refresh_opening_path()
+        self.ids.schema_pick.file_manager.exit_manager()
         self.controller.update_flatbuffers_schema(Path(path))
-
-    def exit_manager_image(self, *args: Any) -> None:
-        """Called when the user reaches the root of the directory tree."""
-        self.file_manager_image.close()
-
-    def exit_manager_inferences(self, *args: Any) -> None:
-        """Called when the user reaches the root of the directory tree."""
-        self.file_manager_inferences.close()
-
-    def exit_manager_flatbuffers(self, *args: Any) -> None:
-        """Called when the user reaches the root of the directory tree."""
-        self.file_manager_flatbuffers.close()
 
     def show_flatbuffers_process_result(self, result: str) -> None:
         MDSnackbar(
