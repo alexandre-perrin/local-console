8a0c211bfaeb5e95bff6ed17bfe9f6e94a4ef4b3
Luis Linares <luis@midokura.com>
2024-04-18 14:59:19 +0200

    feat(utils): add storage size watcher class
    This class has a configurable functionality that keeps
    the cumulative size of a directory's contents under a
    given limit, by deleting the oldest files as needed.

---
+++ tests/unit/utils/test_fswatcher.py
@@ -0,0 +1,131 @@
+import random
+from collections import OrderedDict
+from pathlib import Path
+from unittest.mock import patch
+
+import pytest
+from wedge_cli.utils.fswatch import StorageSizeWatcher
+
+
+@pytest.fixture
+def dir_layout(tmpdir):
+    entries = [
+        tmpdir.join("fileA"),
+        tmpdir.join("fileB"),
+        tmpdir.mkdir("sub").join("file0"),
+        tmpdir.join("sub").mkdir("subsub").join("file0"),
+    ]
+    # Make all entries, files of size 1
+    for e in entries:
+        e.write_binary(b"0")
+
+    return [Path(tmpdir), len(entries)]
+
+
+class walk_entry_mock:
+    def __init__(self) -> None:
+        self.age = 0
+
+    def __call__(self, path: Path) -> tuple[tuple[int, Path], int]:
+        size = 1
+        age = self.age
+        self.age += 1
+        return (age, path), size
+
+
+def create_new(root: Path) -> Path:
+    new_file = root / f"{random.randint(1, 1e6)}"
+    new_file.write_bytes(b"0")
+    return new_file
+
+
+def test_regular_sequence(dir_layout):
+    dir_base, size = dir_layout
+    with patch("wedge_cli.utils.fswatch.walk_entry", walk_entry_mock()):
+        w = StorageSizeWatcher(check_frequency=10)
+        assert w.state == StorageSizeWatcher.State.Start
+
+        w.set_path(dir_base)
+        assert w.state == StorageSizeWatcher.State.Accumulating
+        assert w.storage_usage == size
+
+        w.incoming(create_new(dir_base))
+        assert w.state == StorageSizeWatcher.State.Accumulating
+        assert w.storage_usage == size + 1
+        size += 1
+
+        oldest_age, _ = w.get_oldest()
+        assert oldest_age == 0
+
+        st_limit = 4
+        w.set_storage_limit(st_limit)
+        oldest_age, _ = w.get_oldest()
+        assert oldest_age == 1
+        assert w.storage_usage == st_limit
+        assert w._consistency_check()
+
+        # Test creating and registering new files
+        # in lockstep
+        num_new_files = random.randint(5, 20)
+        for _ in range(num_new_files):
+            w.incoming(create_new(dir_base))
+        assert w._consistency_check()
+        assert w.storage_usage == st_limit
+        expected_oldest_age = oldest_age + num_new_files
+        oldest_age, _ = w.get_oldest()
+        # This is due to the timestamp mocking, as each new file
+        # is timestamped one time unit later than the previous.
+        assert oldest_age == expected_oldest_age
+
+        # Test creating and registering new files
+        # not in lockstep
+        num_new_files = random.randint(5, 20)
+        for _ in range(num_new_files):
+            create_new(dir_base)
+        # although consistency_check restores consistency,
+        # it returns whether state was consistent before
+        assert not w._consistency_check()
+        # hence, a further call should indicate consistency.
+        assert w._consistency_check()
+        expected_oldest_age = oldest_age + num_new_files
+
+        # However, pruning is still necessary:
+        oldest_age, _ = w.get_oldest()
+        assert oldest_age != expected_oldest_age
+        assert w.storage_usage != st_limit
+        w._prune()
+        oldest_age, _ = w.get_oldest()
+        assert oldest_age == expected_oldest_age
+        assert w.storage_usage == st_limit
+
+
+def test_age_bookkeeping():
+    names = "abcdefghijklmn"
+    timestamps = list(range(len(names)))
+    random.shuffle(timestamps)
+
+    # helper dict for building assertions
+    min_timestamp = min(timestamps)
+    max_timestamp = max(timestamps)
+    helper = {
+        timestamp: name
+        for timestamp, name in zip(timestamps, names)
+        if timestamp in (min_timestamp, max_timestamp)
+    }
+    name_of_min_timestamp = helper[min_timestamp]
+    name_of_max_timestamp = helper[max_timestamp]
+
+    odd = OrderedDict(
+        sorted(
+            ((((timestamp, name), None)) for timestamp, name in zip(timestamps, names)),
+            key=lambda e: e[0],
+        )
+    )
+    first_key = next(iter(odd.keys()))
+    assert first_key == (min_timestamp, name_of_min_timestamp)
+
+    last_key, _ = odd.popitem()
+    assert last_key == (max_timestamp, name_of_max_timestamp)
+
+    popped_first, _ = odd.popitem(last=False)
+    assert first_key == popped_first
---
+++ wedge-cli/src/wedge_cli/utils/fswatch.py
@@ -0,0 +1,181 @@
+import enum
+import logging
+from collections import OrderedDict
+from collections.abc import Iterator
+from os import walk
+from pathlib import Path
+from typing import Optional
+
+
+logger = logging.getLogger(__name__)
+
+
+class WatchException(Exception):
+    pass
+
+
+class StorageSizeWatcher:
+    class State(enum.Enum):
+        Start = enum.auto()
+        Accumulating = enum.auto()
+        Checking = enum.auto()
+
+    def __init__(self, check_frequency: int = 50) -> None:
+        """
+        Class for watching a directory for incoming files while maintaining
+        the total storage usage within the directory under a given limit size,
+        pruning the oldest files when necessary.
+
+        Bookkeeping is made in-memory for it to remain fast, however consistency
+        is checked against the filesystem once every given number of incoming
+        files.
+
+        Args:
+                check_frequency (int, optional): check consistency after this many new files. Defaults to 50.
+        """
+        self.check_frequency = check_frequency
+        self._path: Optional[Path] = None
+        self.state = self.State.Start
+        self._size_limit: Optional[int] = None
+        self.content: OrderedDict[tuple[int, Path], int] = OrderedDict()
+        self.storage_usage = 0
+        self._remaining_before_check = self.check_frequency
+
+    def set_path(self, path: Path) -> None:
+        assert path.is_dir()
+
+        p = path.resolve()
+        if p == self._path:
+            return
+        self._path = p
+
+        # Execute regardless of current state
+        self._build_content_dict()
+
+    def set_storage_limit(self, limit: int) -> None:
+        assert limit >= 0
+
+        self._size_limit = limit
+        if self.state == self.State.Accumulating:
+            self._prune()
+
+    def incoming(self, path: Path) -> None:
+        assert path.is_file()
+
+        if not self._path:
+            return
+
+        if not path.resolve().is_relative_to(self._path):
+            raise WatchException(
+                f"Incoming file {path} does not belong to base directory {self._path}"
+            )
+
+        if self.state == self.State.Accumulating:
+            self._register_file(path)
+
+            self._remaining_before_check -= 1
+            if self._remaining_before_check == 0:
+                self._consistency_check()
+            else:
+                self._prune()
+        else:
+            logger.warning(
+                f"Deferring update of size statistic for incoming file {path} during state {self.state}"
+            )
+
+    def get_oldest(self) -> Optional[tuple[int, Path]]:
+        if self.content:
+            key = next(iter(self.content.keys()))
+            assert key
+            return key
+        else:
+            return None
+
+    def _register_file(self, path: Path) -> None:
+        key, val = walk_entry(path)
+        self.content[key] = val
+        self.storage_usage += val
+
+    def _unregister_file(self, path: Path) -> None:
+        key, val = walk_entry(path)
+        self.storage_usage -= val
+        try:
+            self.content.pop(key)
+        except KeyError:
+            stale_keys = [sk for sk in self.content if sk[1] == path]
+            for sk in stale_keys:
+                self.content.pop(sk)
+
+    def _build_content_dict(self) -> None:
+        """
+        Generate a dictionary ordered first by file age, then by file name
+        for disambiguation, with the value being the file size.
+        """
+        assert self._path
+        self.state = self.State.Accumulating
+
+        sorted_e = sorted(
+            (walk_entry(p) for p in walk_files(self._path)), key=lambda e: e[0]
+        )
+        self.content = OrderedDict(sorted_e)
+        self.storage_usage = sum(e[1] for e in sorted_e)
+
+    def _prune(self) -> None:
+        if not self._size_limit:
+            return
+
+        # In order to make this class thread-safe,
+        # the following would be required:
+        # self.state == self.State.Checking
+
+        while self.storage_usage > self._size_limit:
+            (_, path), size = self.content.popitem(last=False)
+            try:
+                path.unlink()
+            except FileNotFoundError:
+                logger.warning(f"File {path} was already removed")
+            self.storage_usage -= size
+
+        # In order to make this class thread-safe,
+        # the following would be required:
+        # self.state == self.State.Accumulating
+
+    def _consistency_check(self) -> bool:
+        assert self._path
+        in_memory = {k[1] for k in self.content.keys()}
+        in_storage = set(walk_files(self._path))
+
+        difference = in_storage - in_memory
+        if difference:
+            logger.warning(
+                f"File bookkeeping inconsistency: new files on disk are: {difference}"
+            )
+            for path in difference:
+                self._register_file(path)
+            return False
+
+        difference = in_memory - in_storage
+        if difference:
+            logger.warning(
+                f"File bookkeeping inconsistency: files unexpectedly removed: {difference}"
+            )
+            for path in difference:
+                self._unregister_file(path)
+            return False
+
+        return True
+
+
+def walk_entry(path: Path) -> tuple[tuple[int, Path], int]:
+    st = path.stat()
+    file_age = st.st_mtime_ns
+    file_size = st.st_size
+    return (file_age, path), file_size
+
+
+def walk_files(root: Path) -> Iterator[Path]:
+    # os.walk to be replaced with:
+    # https://docs.python.org/3.12/library/pathlib.html#pathlib.Path.walk
+    for dir_path, dir_names, file_names in walk(root):
+        for fname in file_names:
+            yield Path(dir_path).joinpath(fname)
