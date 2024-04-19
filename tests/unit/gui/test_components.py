5d4420b45b84aaa1d03ffcf16550e207d15329ac
tonibofarull <toni.bofarull@midokura.com>
2024-04-19 16:17:12 +0200

    feat: define widget to autoscroll

---
+++ tests/unit/gui/test_components.py
@@ -0,0 +1,12 @@
+from kivy.clock import Clock
+from wedge_cli.gui.view.common.components import CodeInputCustom
+
+
+def test_code_input_custom():
+    code_input = CodeInputCustom()
+
+    assert tuple(code_input.cursor) == (0, 0)
+    code_input.text = "my input text"
+    assert tuple(code_input.cursor) != (0, 0)
+    Clock.tick()
+    assert tuple(code_input.cursor) == (0, 0)
