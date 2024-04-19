911ee43c4b518c59124d722fd9a3131f5d7fd36a
Luis Linares <luis@midokura.com>
2024-04-19 16:58:44 +0200

    fix(gui): provide input dead zone for ROI drawing

--- tests/unit/utils/test_roi_mapping.py
+++ tests/unit/utils/test_roi_mapping.py
@@ -1,7 +1,12 @@
+from pytest import approx
 from wedge_cli.gui.utils.axis_mapping import as_normal_in_set
 from wedge_cli.gui.utils.axis_mapping import DEFAULT_ROI
+from wedge_cli.gui.utils.axis_mapping import get_dead_zone_within_image
+from wedge_cli.gui.utils.axis_mapping import get_dead_zone_within_widget
+from wedge_cli.gui.utils.axis_mapping import get_normalized_center_subregion
 from wedge_cli.gui.utils.axis_mapping import pixel_roi_from_normals
 from wedge_cli.gui.utils.axis_mapping import SENSOR_SIZE
+from wedge_cli.gui.utils.axis_mapping import snap_point_in_deadzone
 
 
 def test_normalizing():
@@ -36,3 +41,49 @@ def test_reference_point_denormalization():
     assert v_size == SENSOR_SIZE[1] / 2
     assert h_offset == (SENSOR_SIZE[0] / 2 - 1)
     assert v_offset == (4 * SENSOR_SIZE[1] / 10 - 1)
+
+
+def test_dead_zone_snapping_to_nearest_border():
+    dead_zone_px = 20
+
+    # This case is when the heights are equal
+    widget_size = (400, 200)
+    image_size = (300, 200)
+
+    active_subregion = get_normalized_center_subregion(image_size, widget_size)
+    assert active_subregion[1] == (0, 1)  # This is because heights are equal
+    assert active_subregion[0] == approx((50 / 400, (400 - 50) / 400))
+
+    dead_subregion_w = get_dead_zone_within_widget(
+        dead_zone_px, image_size, widget_size
+    )
+    dead_subregion_i = get_dead_zone_within_image(dead_subregion_w, active_subregion)
+
+    assert dead_subregion_i[1] == approx((20 / 200, (200 - 20) / 200))
+    assert dead_subregion_i[0] == approx(
+        (20 / (400 - 100), ((400 - 100) - 20) / (400 - 100))
+    )
+
+    # Point within the left dead zone
+    pos_normalized = (0.5 * dead_zone_px / image_size[0], 0.5)
+    snapped = snap_point_in_deadzone(pos_normalized, dead_subregion_i)
+    assert snapped[0] == 0
+    assert snapped[1] == 0.5
+
+    # Point within the right dead zone
+    pos_normalized = (1 - 0.5 * (dead_zone_px / image_size[0]), 0.5)
+    snapped = snap_point_in_deadzone(pos_normalized, dead_subregion_i)
+    assert snapped[0] == 1
+    assert snapped[1] == 0.5
+
+    # Point within the bottom dead zone
+    pos_normalized = (0.5, 0.5 * dead_zone_px / image_size[1])
+    snapped = snap_point_in_deadzone(pos_normalized, dead_subregion_i)
+    assert snapped[0] == 0.5
+    assert snapped[1] == 0
+
+    # Point within the top dead zone
+    pos_normalized = (0.5, 1 - 0.5 * (dead_zone_px / image_size[1]))
+    snapped = snap_point_in_deadzone(pos_normalized, dead_subregion_i)
+    assert snapped[0] == 0.5
+    assert snapped[1] == 1
