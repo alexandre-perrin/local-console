0d57ddfe348b1e2c7f6ccf502538f9785d363fb5
tonibofarull <toni.bofarull@midokura.com>
2024-04-22 15:26:59 +0200

    test(driver): improve testing structure

--- tests/unit/gui/test_driver.py
+++ tests/unit/gui/test_driver.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
+import pytest
 from wedge_cli.core.config import config_to_schema
 from wedge_cli.core.config import get_default_config
 from wedge_cli.core.schemas.schemas import AgentConfiguration
@@ -15,6 +16,29 @@ patch("wedge_cli.gui.utils.sync_async.run_on_ui_thread", lambda fn: fn).start() 
 from wedge_cli.gui.driver import Driver  # noqa
 
 
+def get_default_config_as_schema() -> AgentConfiguration:
+    return config_to_schema(get_default_config())
+
+
+def create_new(root: Path) -> Path:
+    new_file = root / f"{random.randint(1, 1e6)}"
+    new_file.write_bytes(b"0")
+    return new_file
+
+
+@pytest.fixture(autouse=True)
+def common_patches():
+    with (
+        patch("wedge_cli.gui.driver.TimeoutBehavior"),
+        # This test does not use Hypothesis' strategies as they are not readily
+        # integrable with the 'tmpdir' fixture, and anyway the functionality tested
+        # by this suite is completely independent on the persistent configuration.
+        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
+        patch("wedge_cli.clients.agent.get_config", get_default_config_as_schema),
+    ):
+        yield
+
+
 def test_file_move(tmpdir):
     origin = Path(tmpdir.join("fileA"))
     origin.write_bytes(b"0")
@@ -24,103 +48,72 @@ def test_file_move(tmpdir):
     assert moved.parent == target
 
 
-def create_new(root: Path) -> Path:
-    new_file = root / f"{random.randint(1, 1e6)}"
-    new_file.write_bytes(b"0")
-    return new_file
-
-
-def get_default_config_as_schema() -> AgentConfiguration:
-    return config_to_schema(get_default_config())
-
-
 def test_storage_paths(tmpdir):
     tgd = Path(tmpdir.mkdir("images"))
 
-    with (
-        patch("wedge_cli.gui.driver.TimeoutBehavior"),
-        # This test does not use Hypothesis' strategies as they are not readily
-        # integrable with the 'tmpdir' fixture, and anyway the functionality tested
-        # by this suite is completely independent on the persistent configuration.
-        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
-        patch("wedge_cli.clients.agent.get_config", get_default_config_as_schema),
-    ):
-        mock_gui = MagicMock()
-        mock_nursery = MagicMock()
-        driver = Driver(mock_gui, mock_nursery)
+    mock_gui = MagicMock()
+    mock_nursery = MagicMock()
+    driver = Driver(mock_gui, mock_nursery)
 
-        # Set default image dir
-        driver.temporary_image_directory = tgd
-        driver.set_image_directory(tgd)
+    # Set default image dir
+    driver.temporary_image_directory = tgd
+    driver.set_image_directory(tgd)
 
-        # Storing an image when image dir has not changed default
-        new_image = create_new(tgd)
-        saved = driver.save_into_image_directory(new_image)
-        assert saved.parent == tgd
+    # Storing an image when image dir has not changed default
+    new_image = create_new(tgd)
+    saved = driver.save_into_image_directory(new_image)
+    assert saved.parent == tgd
 
-        # Change the target image dir
-        new_image_dir = Path(tmpdir.mkdir("another_image_dir"))
-        driver.set_image_directory(new_image_dir)
+    # Change the target image dir
+    new_image_dir = Path(tmpdir.mkdir("another_image_dir"))
+    driver.set_image_directory(new_image_dir)
 
-        # Storing an image when image dir has been changed
-        new_image = create_new(tgd)
-        saved = driver.save_into_image_directory(new_image)
-        assert saved.parent == new_image_dir
+    # Storing an image when image dir has been changed
+    new_image = create_new(tgd)
+    saved = driver.save_into_image_directory(new_image)
+    assert saved.parent == new_image_dir
 
 
 def test_save_into_image_directory(tmpdir):
     root = Path(tmpdir)
     tgd = root / "notexists"
 
-    with (
-        patch("wedge_cli.gui.driver.TimeoutBehavior"),
-        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
-        patch("wedge_cli.gui.driver.Agent"),
-    ):
-        driver = Driver(MagicMock(), MagicMock())
+    driver = Driver(MagicMock(), MagicMock())
 
-        assert not tgd.exists()
-        driver.temporary_image_directory = tgd
-        driver.set_image_directory(tgd)
-        assert tgd.exists()
+    assert not tgd.exists()
+    driver.temporary_image_directory = tgd
+    driver.set_image_directory(tgd)
+    assert tgd.exists()
 
-        tgd.rmdir()
+    tgd.rmdir()
 
-        assert not tgd.exists()
-        driver.save_into_image_directory(create_new(root))
-        assert tgd.exists()
+    assert not tgd.exists()
+    driver.save_into_image_directory(create_new(root))
+    assert tgd.exists()
 
 
 def test_save_into_inferences_directory(tmpdir):
     root = Path(tmpdir)
     tgd = root / "notexists"
 
-    with (
-        patch("wedge_cli.gui.driver.TimeoutBehavior"),
-        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
-        patch("wedge_cli.gui.driver.Agent"),
-    ):
-        driver = Driver(MagicMock(), MagicMock())
+    driver = Driver(MagicMock(), MagicMock())
 
-        assert not tgd.exists()
-        driver.temporary_inference_directory = tgd
-        driver.set_inference_directory(tgd)
-        assert tgd.exists()
+    assert not tgd.exists()
+    driver.temporary_inference_directory = tgd
+    driver.set_inference_directory(tgd)
+    assert tgd.exists()
 
-        tgd.rmdir()
+    tgd.rmdir()
 
-        assert not tgd.exists()
-        driver.save_into_inferences_directory(create_new(root))
-        assert tgd.exists()
+    assert not tgd.exists()
+    driver.save_into_inferences_directory(create_new(root))
+    assert tgd.exists()
 
 
 def test_process_camera_upload_images(tmpdir):
     root = Path(tmpdir)
 
     with (
-        patch("wedge_cli.gui.driver.TimeoutBehavior"),
-        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
-        patch("wedge_cli.gui.driver.Agent"),
         patch.object(
             Driver, "save_into_image_directory"
         ) as mock_save_into_image_directory,
@@ -139,9 +132,6 @@ def test_process_camera_upload_inferences(tmpdir):
     root = Path(tmpdir)
 
     with (
-        patch("wedge_cli.gui.driver.TimeoutBehavior"),
-        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
-        patch("wedge_cli.gui.driver.Agent"),
         patch.object(
             Driver, "save_into_inferences_directory"
         ) as mock_save_into_inferences_directory,
@@ -160,13 +150,10 @@ def test_process_camera_upload_inferences(tmpdir):
         mock_update_inference_data_flatbuffers.assert_not_called()
 
 
-def test_process_camera_upload_inferences(tmpdir):
+def test_process_camera_upload_inferences_with_fb(tmpdir):
     root = Path(tmpdir)
 
     with (
-        patch("wedge_cli.gui.driver.TimeoutBehavior"),
-        patch("wedge_cli.gui.driver.get_config", get_default_config_as_schema),
-        patch("wedge_cli.gui.driver.Agent"),
         patch.object(
             Driver, "save_into_inferences_directory"
         ) as mock_save_into_inferences_directory,
