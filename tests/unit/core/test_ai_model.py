e73a3a57faee4be5f2cbece9c0d84e1b35fb84f7
Luis Linares <luis@midokura.com>
2024-07-11 01:51:28 +0200

    refactor(ai_model): part 4/4: move deployment into core
    Given that previous commits moved all variables involved in the
    deployment state machine of an AI model, into the CameraState
    class, this state machine is no longer bound to objects in the
    screen's model or view. Hence, this commit moves it into a
    new submodule of /core/camera, and the remaining code in the
    controller class is drastically reduced. The unit test now
    becomes for the deployment state machine, so its location within
    the tests source tree is moved accordingly.

--- tests/unit/core/test_ai_model.py
+++ tests/unit/core/test_ai_model.py
@@ -13,23 +13,18 @@
 # limitations under the License.
 #
 # SPDX-License-Identifier: Apache-2.0
-import sys
 from unittest.mock import AsyncMock
 from unittest.mock import MagicMock
 from unittest.mock import patch
 
 import pytest
+from local_console.core.camera import CameraState
+from local_console.core.camera.ai_model import deploy_step
+from local_console.core.camera.ai_model import undeploy_step
+from local_console.core.commands.ota_deploy import get_package_hash
 from local_console.core.config import config_to_schema
 from local_console.core.config import get_default_config
-
-sys.modules["local_console.gui.driver"] = MagicMock()
-
-from local_console.gui.controller.ai_model_screen import AIModelScreenController
-from local_console.core.commands.ota_deploy import get_package_hash
 from local_console.utils.local_network import get_my_ip_by_routing
-
-# To allow other tests to load driver
-del sys.modules["local_console.gui.driver"]
 
 
 @pytest.fixture(params=["Done", "Failed"])
@@ -49,7 +44,7 @@ def mock_get_config():
 @pytest.fixture(autouse=True)
 def fixture_get_config():
     with patch(
-        "local_console.gui.controller.ai_model_screen.get_config",
+        "local_console.core.camera.ai_model.get_config",
         mock_get_config,
     ) as _fixture:
         yield _fixture
@@ -57,18 +52,16 @@ def fixture_get_config():
 
 @pytest.mark.trio
 async def test_undeploy_step_rpc_sent(network_id: str):
-    mock_model = AsyncMock()
-    mock_model.ota_event = AsyncMock()
-    mock_driver = MagicMock()
+    camera_state = CameraState()
     mock_agent = MagicMock()
     mock_agent.mqtt_scope.return_value = AsyncMock()
     mock_agent.configure = AsyncMock()
-    with patch(
-        "local_console.gui.controller.ai_model_screen.Agent", return_value=mock_agent
+    with (
+        patch("local_console.core.camera.ai_model.Agent", return_value=mock_agent),
+        patch.object(camera_state, "device_config") as mock_config,
     ):
-        mock_model.device_config.OTA.UpdateStatus = "Done"
-        controller = AIModelScreenController(mock_model, mock_driver, MagicMock())
-        await controller.undeploy_step(network_id)
+        mock_config.value.OTA.UpdateStatus = "Done"
+        await undeploy_step(camera_state, network_id, MagicMock())
         payload = (
             f'{{"OTA":{{"UpdateModule":"DnnModel","DeleteNetworkID":"{network_id}"}}}}'
         )
@@ -79,19 +72,18 @@ async def test_undeploy_step_rpc_sent(network_id: str):
 
 @pytest.mark.trio
 async def test_undeploy_step_not_deployed_model(update_status: str):
-    mock_model = AsyncMock()
-    mock_model.ota_event = AsyncMock()
-    mock_driver = MagicMock()
+    camera_state = CameraState()
     mock_agent = MagicMock()
     mock_agent.mqtt_scope.return_value = AsyncMock()
     mock_agent.configure = AsyncMock()
-    with patch(
-        "local_console.gui.controller.ai_model_screen.Agent", return_value=mock_agent
+    with (
+        patch("local_console.core.camera.ai_model.Agent", return_value=mock_agent),
+        patch.object(camera_state, "device_config") as mock_config,
+        patch.object(camera_state, "ota_event") as mock_ota_event,
     ):
-        mock_model.device_config.OTA.UpdateStatus = update_status
-        controller = AIModelScreenController(mock_model, mock_driver, MagicMock())
-        await controller.undeploy_step("000001")
-        mock_model.ota_event.assert_not_awaited()
+        mock_config.value.OTA.UpdateStatus = update_status
+        await undeploy_step(camera_state, "000001", MagicMock())
+        mock_ota_event.assert_not_awaited()
 
 
 @pytest.mark.trio
@@ -99,31 +91,28 @@ async def test_deploy_step(tmp_path, network_id, update_status: str):
     filename = "dummy.bin"
     tmp_file = tmp_path / filename
 
-    mock_model = AsyncMock()
-    mock_model.ota_event = AsyncMock()
-    mock_driver = MagicMock()
+    camera_state = CameraState()
     mock_agent = MagicMock()
     mock_agent.mqtt_scope.return_value = AsyncMock()
     mock_agent.configure = AsyncMock()
+
     with (
+        patch("local_console.core.camera.ai_model.Agent", return_value=mock_agent),
+        patch.object(camera_state, "device_config") as mock_config,
+        patch.object(camera_state, "ota_event") as mock_ota_event,
         patch(
-            "local_console.gui.controller.ai_model_screen.Agent",
+            "local_console.core.camera.ai_model.AsyncWebserver",
             return_value=mock_agent,
         ),
         patch(
-            "local_console.gui.controller.ai_model_screen.AsyncWebserver",
-            return_value=mock_agent,
-        ),
-        patch(
-            "local_console.gui.controller.ai_model_screen.get_network_ids",
+            "local_console.core.camera.ai_model.get_network_ids",
             return_value=[network_id],
         ),
     ):
-        mock_model.device_config.OTA.UpdateStatus = update_status
-        controller = AIModelScreenController(mock_model, mock_driver, MagicMock())
+        mock_config.value.OTA.UpdateStatus = update_status
         with open(tmp_file, "w") as f:
             f.write("dummy")
-        await controller.deploy_step(network_id, tmp_file)
+        await deploy_step(camera_state, network_id, tmp_file, MagicMock())
         hashvalue = get_package_hash(tmp_file)
         payload = (
             '{"OTA":{"UpdateModule":"DnnModel","DesiredVersion":"",'
@@ -134,4 +123,4 @@ async def test_deploy_step(tmp_path, network_id, update_status: str):
         mock_agent.configure.assert_called_once_with(
             "backdoor-EA_Main", "placeholder", payload
         )
-        mock_model.ota_event.assert_not_awaited()
+        mock_ota_event.assert_not_awaited()
