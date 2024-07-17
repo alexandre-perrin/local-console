c41c3e6ffc358e1cedb04ff08d922859eadc4890
Luis Linares <luis@midokura.com>
2024-07-17 12:11:47 +0200

    refactor(gui-streaming): use common display_error method
    Signed-off-by: Luis Linares <luis@midokura.com>

--- local-console/src/local_console/gui/view/streaming_screen/streaming_screen.py
+++ local-console/src/local_console/gui/view/streaming_screen/streaming_screen.py
@@ -13,14 +13,9 @@
 # limitations under the License.
 #
 # SPDX-License-Identifier: Apache-2.0
-from kivy.metrics import dp
 from kivy.uix.codeinput import (
     CodeInput,
 )  # nopycln: import # Required by the screen's KV spec file
-from kivymd.uix.snackbar import MDSnackbar
-from kivymd.uix.snackbar import MDSnackbarButtonContainer
-from kivymd.uix.snackbar import MDSnackbarCloseButton
-from kivymd.uix.snackbar import MDSnackbarSupportingText
 from local_console.core.camera import StreamStatus
 from local_console.gui.utils.axis_mapping import DEFAULT_ROI
 from local_console.gui.utils.axis_mapping import pixel_roi_from_normals
@@ -39,7 +34,7 @@ class StreamingScreenView(BaseScreenView):
     def control_roi(self) -> None:
         roi_state: ROIState = self.ids.stream_image.state
         if roi_state == ROIState.Disabled:
-            inform_roi_is_disabled()
+            self.inform_roi_is_disabled()
         elif roi_state in (ROIState.Enabled, ROIState.Viewing):
             self.ids.stream_image.start_roi_draw()
         else:
@@ -51,7 +46,7 @@ class StreamingScreenView(BaseScreenView):
             self.ids.stream_image.cancel_roi_draw()
             self.controller.set_roi(DEFAULT_ROI)
         else:
-            inform_roi_is_disabled()
+            self.inform_roi_is_disabled()
 
     def refresh_roi_state(self, roi_state: ROIState) -> None:
         # The "Set ROI" button
@@ -81,20 +76,5 @@ class StreamingScreenView(BaseScreenView):
         self.ids.lbl_roi_v_offset.text = str(v_offset)
         self.ids.lbl_roi_v_size.text = str(v_size)
 
-
-def inform_roi_is_disabled() -> None:
-    MDSnackbar(
-        MDSnackbarSupportingText(
-            text="Cannot set ROI without having received a camera frame",
-        ),
-        MDSnackbarButtonContainer(
-            MDSnackbarCloseButton(
-                icon="close",
-            ),
-            pos_hint={"center_y": 0.5},
-        ),
-        y=dp(24),
-        orientation="horizontal",
-        pos_hint={"center_x": 0.5},
-        size_hint_x=0.5,
-    ).open()
+    def inform_roi_is_disabled(self) -> None:
+        self.display_error("Cannot set ROI without having received a camera frame")
