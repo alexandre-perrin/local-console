5ad08e9f6845f175c576b5db1f4b9f58d9da355a
tonibofarull <toni.bofarull@midokura.com>
2024-08-07 16:25:37 +0200

    feat: helper function to run webserver in ui thread

--- local-console/src/local_console/core/camera/mixin_streaming.py
+++ local-console/src/local_console/core/camera/mixin_streaming.py
@@ -156,7 +156,7 @@ class StreamingMixin(HasMQTTset, IsAsyncReady):
         """
         with (TemporaryDirectory(prefix="LocalConsole_") as tempdir,):
             async with AsyncWebserver(
-                Path(tempdir), port=0, on_incoming=self._process_camera_upload
+                Path(tempdir), port=0, on_incoming=self.__process_camera_upload
             ) as image_serve:
 
                 logger.info(f"Uploading data into {tempdir}")
@@ -177,14 +177,19 @@ class StreamingMixin(HasMQTTset, IsAsyncReady):
                 await trio.sleep_forever()
 
     @run_on_ui_thread
+    def __process_camera_upload(self, incoming_file: Path) -> None:
+        self._process_camera_upload(incoming_file)
+
     def _process_camera_upload(self, incoming_file: Path) -> None:
         if incoming_file.suffix.lstrip(".") == self._extension_infers:
+            assert self.inference_dir_path.value
             target_dir = Path(self.inference_dir_path.value)
             assert target_dir
             final_file = self._save_into_input_directory(incoming_file, target_dir)
             self._grouper.register(final_file, final_file)
 
         elif incoming_file.suffix.lstrip(".") == self._extension_images:
+            assert self.image_dir_path.value
             target_dir = Path(self.image_dir_path.value)
             assert target_dir
             final_file = self._save_into_input_directory(incoming_file, target_dir)
